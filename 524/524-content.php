a074764b2950562b5091e8f1538da6d7c2f0e8a4
tdc-students <phamchilinh15112016@gmail.com>
2019-09-26 09:13:05 +0700

    Sua Module

--- 524/524-content.php
+++ 524/524-content.php
@@ -1,13 +1,22 @@
 <?php
-$url_host = 'http://' . $_SERVER['HTTP_HOST'];
-$pattern_document_root = addcslashes(realpath($_SERVER['DOCUMENT_ROOT']), '\\');
-$pattern_uri = '/' . $pattern_document_root . '(.*)$/';
 
-preg_match_all($pattern_uri, __DIR__, $matches);
-$url_path = $url_host . $matches[1][0];
-$url_path = str_replace('\\', '/', $url_path);
+	$url_host = 'http://'.$_SERVER['HTTP_HOST'];
+	$pattern_document_root = addcslashes(realpath($_SERVER['DOCUMENT_ROOT']), '\\');
+	$pattern_uri = '/' . $pattern_document_root . '(.*)$/';
+	
+	preg_match_all($pattern_uri, __DIR__, $matches);
+	$url_path = $url_host . $matches[1][0];
+	$url_path = str_replace('\\', '/', $url_path);
+
+	if (!class_exists('lessc')) {
+		$dir_block = dirname($_SERVER['SCRIPT_FILENAME']);		
+		require_once($dir_block.'/libs/lessc.inc.php');
+	}
+	
+	$less = new lessc;
+	$less->compileFile('less/524.less', 'css/524.css');
+	
 ?>
-
 <!-- Header-->
 <div class="type_524">
     <div class="container menu-container-wrapper">
